f3be023581de6ec56ef4a25d2a11239e5467c0fe
nepthune-enpc <nepthune.enpc@gmail.com>
2023-07-17 10:36:54 +0100

    first commit

---
+++ cofig.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_EEG_PATH = "../../../Downloads/A00062219/A00062219"
+EEG_CSV_PATH = os.path.join(ROOT_EEG_PATH, "EEG/preprocessed/csv_format/gp_A00062219009.csv")
+EEG_EVENTS_PATH = os.path.join(ROOT_EEG_PATH, "EEG/preprocessed/csv_format/gp_A00062219009_events.csv")        
+        
